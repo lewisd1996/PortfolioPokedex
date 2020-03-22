445517ca358d838abb9ed9b279af7f1810cca28d
lewisd1996 <lewis.d1996@gmail.com>
2020-03-22 16:51:18 +0000

    changed title of app to PokeDex again

---
+++ build/precache-manifest.8979302f22ac86e8016373dca5a3727b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea8a1f1d2de1b08b51c5af4d6b61c827",
+    "url": "/PortfolioPokedex/index.html"
+  },
+  {
+    "revision": "b590fd7a67a8b6c9f27d",
+    "url": "/PortfolioPokedex/static/css/2.7ad897a5.chunk.css"
+  },
+  {
+    "revision": "e76bdcfa79fd9edc2967",
+    "url": "/PortfolioPokedex/static/css/main.9e3f97d2.chunk.css"
+  },
+  {
+    "revision": "b590fd7a67a8b6c9f27d",
+    "url": "/PortfolioPokedex/static/js/2.0d1d3bed.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/PortfolioPokedex/static/js/2.0d1d3bed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e76bdcfa79fd9edc2967",
+    "url": "/PortfolioPokedex/static/js/main.eaf85b16.chunk.js"
+  },
+  {
+    "revision": "3d135e68167b3f535f3f",
+    "url": "/PortfolioPokedex/static/js/runtime-main.2d371e2b.js"
+  },
+  {
+    "revision": "9a1861aee160e967fca02092a869de18",
+    "url": "/PortfolioPokedex/static/media/spinner.9a1861ae.gif"
+  }
+]);
